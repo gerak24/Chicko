d5103ddc71758909c3b036a338e5f02198e89636
gerak24 <gerak24032003@yandex.ru>
2023-05-18 17:36:24 +0300

    works

--- react-intro/src/Components/Footer/Footer.js
+++ react-intro/src/Components/Footer/Footer.js
@@ -4,7 +4,20 @@ import styles from './Footer.module.scss'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            
+            <div className={styles.content_wrapper}>
+                <div className={styles.workTime}>
+                    ВРЕМЯ РАБОТЫ<br/>
+                    Понедельник-пятница: с 8.00 до 17.00<br/>
+                    Перерыв: с 12.00 до 13.00<br/>
+                    Суббота, воскресенье: выходной<br/>
+                </div>
+                <div className={styles.workTime}>
+                    ООО «Антей»: запчасти для комбайнов, <br/>
+                    ремонт комбайнов, дефектовка комбайнов,<br/>
+                    ремонт и замена двигателя.<br/>
+                    +7 (863) 303-04-41<br/>
+                </div>
+            </div>
         </div>
     );
 };
